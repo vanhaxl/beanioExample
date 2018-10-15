91d2b9e61a54e0baa069b94dd7406b6bfdeec072
Van Ha Nguyen <vanhaxl@gmail.com>
2018-10-15 15:00:30 -0500

    first commit

---
+++ src/main/java/com/vhnguyen/beanio/beanioexample/BeanioexampleApplication.java
@@ -0,0 +1,12 @@
+package com.vhnguyen.beanio.beanioexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanioexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanioexampleApplication.class, args);
+	}
+}
